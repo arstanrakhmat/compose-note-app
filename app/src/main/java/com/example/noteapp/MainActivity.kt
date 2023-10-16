63ff36452f4d225ea70ecaa87f3f44b4f507da95
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-10-16 19:38:52 +0300

    Add Hilt DI and Room into the app

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val noteViewModel: NoteViewModel by viewModels()
+                    val noteViewModel = viewModel<NoteViewModel>()
                     NotesApp(noteViewModel = noteViewModel)
                 }
             }
@@ -36,8 +36,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
-    val noteList = noteViewModel.getAllNotes()
+fun NotesApp(noteViewModel: NoteViewModel) {
+    val noteList = noteViewModel.noteList.collectAsState().value
 
     NoteScreen(
         notes = noteList,
--- app/src/main/java/com/example/noteapp/screen/NoteViewModel.kt
+++ app/src/main/java/com/example/noteapp/screen/NoteViewModel.kt
@@ -1,25 +1,41 @@
 package com.example.noteapp.screen
 
-import androidx.compose.runtime.mutableStateListOf
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.noteapp.model.Note
-import com.example.noteapp.data.NoteDataSource
+import com.example.noteapp.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NoteViewModel : ViewModel() {
-    private var noteList = mutableStateListOf<Note>()
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+
+    private val _noteList = MutableStateFlow<List<Note>>(emptyList())
+    val noteList = _noteList.asStateFlow()
 
     init {
-        noteList.addAll(NoteDataSource().loadNotes())
-    }
-    fun addNote(note: Note) {
-        noteList.add(note)
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllNotes().distinctUntilChanged()
+                .collect { listOfNotes ->
+                    if (listOfNotes.isNullOrEmpty()) {
+                        Log.d("Empty_List", "Empty list")
+                    } else {
+                        _noteList.value = listOfNotes
+                    }
+                }
+        }
     }
 
-    fun removeNote(note: Note) {
-        noteList.remove(note)
-    }
 
-    fun getAllNotes(): List<Note> {
-        return noteList
-    }
+    fun addNote(note: Note) = viewModelScope.launch { repository.addNote(note) }
+
+    fun updateNote(note: Note) = viewModelScope.launch { repository.updateNote(note) }
+
+    fun removeNote(note: Note) = viewModelScope.launch { repository.deleteNote(note) }
 }
