24f18314d30dfaca0f9cba4c1fda224c6715131c
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-10-16 17:14:23 +0300

    Create note app with dummy implementation

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.noteapp.screen.NoteScreen
+import com.example.noteapp.screen.NoteViewModel
 import com.example.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +25,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoteScreen()
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel = noteViewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val noteList = noteViewModel.getAllNotes()
+
+    NoteScreen(
+        notes = noteList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        }
+    )
 }
 
 
---
+++ app/src/main/java/com/example/noteapp/data/NoteData.kt
@@ -0,0 +1,18 @@
+package com.example.noteapp.data
+
+class NoteDataSource {
+    fun loadNotes(): List<Note> {
+        return listOf(
+            Note(title = "Da1", description = "Do what you need"),
+            Note(title = "Da2", description = "Do what you need"),
+            Note(title = "Da3", description = "Do what you need"),
+            Note(title = "Da4", description = "Do what you need"),
+            Note(title = "Da5", description = "Do what you need"),
+            Note(title = "Da6", description = "Do what you need"),
+            Note(title = "Da7", description = "Do what you need"),
+            Note(title = "Da8", description = "Do what you need"),
+            Note(title = "Da9", description = "Do what you need"),
+            Note(title = "Da10", description = "Do what you need"),
+        )
+    }
+}
